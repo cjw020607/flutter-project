3ec3998b4bf280af89cef6ec12cc12713f8da582
cjw020607 <80667141+cjw020607@users.noreply.github.com>
2023-01-07 15:15:28 +0900

    1-7

--- flutter_application_1/lib/main.dart
+++ flutter_application_1/lib/main.dart
@@ -2,3 +2,15 @@ import 'package:flutter/material.dart';
 
 void main() => runApp(MyApp()); //runApp() 안에 위젯
 
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'First app',
+      theme: ThemeData(primarySwatch: Colors.blue),
+      home: MyHomePage(),
+    );
+  }
+}

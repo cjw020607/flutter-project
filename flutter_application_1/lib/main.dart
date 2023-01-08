e81d571508262f53e9d62edaa8be71ed1f213017
cjw020607 <80667141+cjw020607@users.noreply.github.com>
2023-01-08 16:36:44 +0900

    1-19 without builder widget

--- flutter_application_1/lib/main.dart
+++ flutter_application_1/lib/main.dart
@@ -21,27 +21,38 @@ class MyPage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-        appBar: AppBar(
-          title: Text('Snack Bar'),
-          centerTitle: true,
-        ),
-        body: Center(
-          //TextButton 입체감 없이
-          child: TextButton(
-            child: Text(
-              'Show me',
-              style: TextStyle(color: Colors.white),
-            ),
-            style: TextButton.styleFrom(
-                //primary: Colors.red,
-                backgroundColor: Colors.red),
-            onPressed: () {
-              //something.of(context): 주어진 context에서 위로 올라가면서 가장 가까운 something 찾아서 반환
-              ScaffoldMessenger.of(context).showSnackBar(SnackBar(
-                content: Text('Hellow'),
-              ));
-            },
-          ),
-        ));
+      appBar: AppBar(
+        title: Text('Snack Bar'),
+        centerTitle: true,
+      ),
+      body: MySnackBar(),
+    );
+  }
+}
+
+//업데이트 전 builder 없이 snack bar 만들기
+class MySnackBar extends StatelessWidget {
+  const MySnackBar({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Center(
+      child: ElevatedButton(
+          child: Text('Show me'),
+          style: ElevatedButton.styleFrom(backgroundColor: Colors.blue),
+          onPressed: () {
+            ScaffoldMessenger.of(context).showSnackBar(
+              SnackBar(
+                content: Text(
+                  'Hellow',
+                  textAlign: TextAlign.center,
+                  style: TextStyle(color: Colors.white),
+                ),
+                backgroundColor: Colors.teal,
+                duration: Duration(milliseconds: 1000),
+              ),
+            );
+          }),
+    );
   }
 }

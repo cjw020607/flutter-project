880dcb860f77d00b05c253de149f781e65f5b4b8
cjw020607 <80667141+cjw020607@users.noreply.github.com>
2023-01-09 18:46:01 +0900

    1-20

--- flutter_application_1/lib/main.dart
+++ flutter_application_1/lib/main.dart
@@ -1,5 +1,4 @@
 import 'package:flutter/material.dart';
-import 'package:fluttertoast/fluttertoast.dart';
 
 void main() => runApp(MyApp());
 
@@ -22,32 +21,24 @@ class MyPage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(
-        title: Text('Toast messsage'),
-        centerTitle: true,
-      ),
-      body: Center(
-        child: TextButton(
-          onPressed: () {
-            flutterToast();
-          },
-          child: Text('Toast'),
-          style: TextButton.styleFrom(
-            backgroundColor: Colors.blue,
-            primary: Colors.black,
+      backgroundColor: Colors.blue,
+      body: SafeArea(
+        //container는 최대한 많은 공간 차지하려고 함
+        //child 가지게 될 경우 child 크기로 줄어듦 (container는 하나의 child만 가짐)
+        child: Container(
+          color: Colors.red,
+          width: 100,
+          height: 100,
+          //margin: 위젯의 바깥쪽
+          margin: EdgeInsets.symmetric(
+            vertical: 50,
+            horizontal: 10,
           ),
+          //padding: 위젯의 안쪽
+          padding: EdgeInsets.all(40),
+          child: Text('Hello'),
         ),
       ),
     );
   }
-}
-
-void flutterToast() {
-  Fluttertoast.showToast(
-      msg: 'Flutter',
-      gravity: ToastGravity.BOTTOM,
-      backgroundColor: Colors.redAccent,
-      fontSize: 20.0,
-      textColor: Colors.white,
-      toastLength: Toast.LENGTH_SHORT);
 }

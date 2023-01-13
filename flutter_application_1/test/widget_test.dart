d36aec32d3fd912aed8ce2e359bf86bbe388ce53
cjw020607 <80667141+cjw020607@users.noreply.github.com>
2023-01-13 21:51:27 +0900

    2-8 async, await

--- flutter_application_1/test/widget_test.dart
+++ flutter_application_1/test/widget_test.dart
@@ -4,10 +4,11 @@ void main() {
   showData();
 }
 
-void showData() {
+void showData() async {
   startTask();
-  accessData();
-  fetchData();
+  //실행이 끝날때까지 기다림
+  String account = await accessData();
+  fetchData(account);
 }
 
 void startTask() {
@@ -15,25 +16,26 @@ void startTask() {
   print(info1);
 }
 
-void accessData() {
-  //3초 지연
-  // sleep(time);
+//무조건 전 단계 실행 후 다음 단계로 넘어감
+Future<String> accessData() async {
+  String account;
 
   Duration time = Duration(seconds: 3);
 
   if (time.inSeconds > 2) {
-    //accessData 지연되는동안 fetchData 먼저 프린트 됨
-    Future.delayed(time, () {
-      String info2 = '데이터에 처리 완료';
-      print(info2);
+    //Futre.delayed 실행이 끝날때까지 기다림
+    await Future.delayed(time, () {
+      account = '8,500만원';
+      print(account);
     });
   } else {
     String info2 = '데이터를 가져왔습니다.';
     print(info2);
   }
+  return account;
 }
 
-void fetchData() {
-  String info3 = '잔액은 8,500만원 입니다.';
+void fetchData(String account) {
+  String info3 = '잔액은 $account 입니다.';
   print(info3);
 }
